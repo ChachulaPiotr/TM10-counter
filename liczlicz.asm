faa663b84c9bf818ea5ef39c375ce067b56e179a
ChachulaPiotr <33399325+ChachulaPiotr@users.noreply.github.com>
2018-04-18 17:29:51 +0200

    Add files via upload
    Working!!

---
+++ liczlicz.asm
@@ -0,0 +1,73 @@
+ORG 1800h
+START:
+	LD SP,1900h
+	JR enable
+
+ds 	0x1838-$,0
+Interrupt:
+	XOR	A
+	IN	A,(00h)
+	AND	15
+	LD	B,A
+	OUT	(00h),A
+	IN	A,(01h)
+	LD	C,A
+	EI
+	RETI
+enable:
+	IM 1
+	XOR	A
+	LD	B, A
+	EI
+ini:
+	OUT	(00h),A
+	LD	D,64
+	LD	H,255
+main:
+	IN	A,(01h)
+	BIT	4,A
+	JR	Z,zera
+jedynki:
+	DEC	H
+	LD	D,64
+	XOR	A
+	CP	H
+	JR	NZ, main
+	LD	A,9
+	CP	B
+	JR	Z, reload
+	LD	A,C
+	BIT	4,A
+	JR	NZ,time
+	INC 	B
+	LD	A,B
+time:
+	OUT	(00h),A
+	XOR	A
+	LD	C,A
+	LD	D,255
+	IN	A,(01h)
+	BIT	4,A
+	JR	NZ,time
+	JP 	zera2
+zera:
+	IN	A,(01h)
+	BIT	4,A
+	JR	NZ,jedynki
+	DEC 	D
+	XOR	A
+	CP	D
+	JR	NZ, main
+	JP	ini
+zera2:
+	IN	A,(01h)
+	BIT	4,A
+	JR	NZ,time
+	DEC 	D
+	XOR	A
+	CP	D
+	JR	NZ, zera2
+	JP	ini
+reload:
+	LD 	B,0
+	JP	time
